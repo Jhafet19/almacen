14738d0d21b80f2a064e9e18c2c305ef563fc74b
Jhafet19 <20223tn107@utez.edu.mx>
2024-01-12 10:44:31 -0600

    YA tiene conexion

--- src/main/java/mx/edu/utez/almacen/model/inventory/InventoryBean.java
+++ src/main/java/mx/edu/utez/almacen/model/inventory/InventoryBean.java
@@ -1,8 +1,35 @@
 package mx.edu.utez.almacen.model.inventory;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+
+@Entity
+//Le ponemos nombre
+@Table(name="category")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
 public class InventoryBean {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(columnDefinition = "DATE",nullable = false)
+    private LocalDate expiration;
+
+    @Column(nullable = false)
+    private  Integer stock;
+
+    @Column(nullable = false)
+    private  Double pricePublic;
 
 
 }
--- src/main/java/mx/edu/utez/almacen/model/ticket/TicketBean.java
+++ src/main/java/mx/edu/utez/almacen/model/ticket/TicketBean.java
@@ -1,4 +1,40 @@
 package mx.edu.utez.almacen.model.ticket;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+
+@Entity
+//Le ponemos nombre
+@Table(name="ticket")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class TicketBean {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(columnDefinition = "DATE",nullable = false)
+    private LocalDate saleDate;
+
+    @Column(nullable = false)
+    private Integer folio;
+
+    @Column(columnDefinition = "TIME")
+    private LocalTime saleHour;
+
+    @Column(nullable = false)
+    private double totPay;
+
+    @Column(nullable = false)
+    private  Integer totProduct;
 }
