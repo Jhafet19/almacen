f16ab605933814c7cce62eb744d9a354f1675a57
Jhafet19 <20223tn107@utez.edu.mx>
2024-01-19 10:21:19 -0600

    Repositorios

---
+++ src/main/java/mx/edu/utez/almacen/service/rol/RolService.java
@@ -0,0 +1,25 @@
+package mx.edu.utez.almacen.service.rol;
+
+import jakarta.persistence.NamedStoredProcedureQueries;
+import lombok.AllArgsConstructor;
+import mx.edu.utez.almacen.config.ApiResponse;
+import mx.edu.utez.almacen.model.rol.RolRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@AllArgsConstructor
+//Data Trasnfer Object
+public class RolService {
+
+    private final RolRepository repository;
+
+    public ResponseEntity<ApiResponse> getAll() {
+        return new ResponseEntity<>(new ApiResponse(repository.findAll(),
+                HttpStatus.OK), HttpStatus.OK);
+    }
+
+}
