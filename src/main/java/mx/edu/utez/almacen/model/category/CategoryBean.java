6df87cdc7c0624eaffc094f3e6537486a7cbb2e0
Jhafet19 <20223tn107@utez.edu.mx>
2024-02-15 13:25:48 -0600

    PRoobando

--- src/main/java/mx/edu/utez/almacen/model/category/CategoryBean.java
+++ src/main/java/mx/edu/utez/almacen/model/category/CategoryBean.java
@@ -20,6 +20,7 @@ import java.util.Set;
 @Setter
 public class CategoryBean {
     //Colocamos la llave foranea
+    //probando mis commits
    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
